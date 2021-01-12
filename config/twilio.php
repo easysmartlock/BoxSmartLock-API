5742fe88c766ba919eae33f5c27c16c1002ef609
lalaina <lala.misa.09@googlemail.com>
2021-01-12 14:56:08 +0300

    fix twilio ENV

--- config/twilio.php
+++ config/twilio.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'token' => '9760b9fc4cbe7588a6fba8f0631488d4',
-    'sid' => 'ACe229225c2522fc9720d18f37e2f1e145',
-    'from' => '+15109451938'
+    'token' => env('TWILIO_TOKEN','9760b9fc4cbe7588a6fba8f0631488d4'),
+    'sid' => env('TWILIO_SID','ACe229225c2522fc9720d18f37e2f1e145'),
+    'from' => env('TWILIO_FROM','+15109451938')
 ];
